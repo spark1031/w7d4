54ebcf4b5cc8a581f4c80304ae00d30d854bab9e
spark1031 <sujinpark1031@gmail.com>
2018-12-05 16:31:54 -0800

    Complete redux cycle and Root component

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+
+// import * as APIUtil from './util/api_util';
+// import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+// import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
-  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  const store = configureStore();
+  ReactDOM.render(<Root store={store}/>, rootEl);
 });
+
